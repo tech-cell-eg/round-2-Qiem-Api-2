44f102bd64b313fb2b1ff096d64f61d1849074e3
BasmalaBrham <basmala.brham@gmail.com>
2025-02-04 15:01:32 +0200

    Implemented notification system for inspectors and evaluation companies, and created Terms & Conditions page.

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,13 +10,24 @@ class NotificationController extends Controller
 {
     //to send notification
     public function sendNotification(Request $request){
-        $user=User::find($request->input('user_id'));
-        if(!$user){
-            return response()->json(['error'=>'User not found']);
+        //Validate the incoming request data
+        $validated = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'title' => 'required|string|max:255',
+            'message' => 'required|string'
+        ]);
+
+        // Find the user by ID
+        $user = User::find($request->input('user_id'));
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
         }
-         //to send new notification to user
-         $user->notify(new customNotification($request->input('title'), $request->input('message')));
-        return response()->json(['success'=>'Notification sent successsfully']);
+
+        // Send the notification
+        $user->notify(new CustomNotification($request->input('title'), $request->input('message')));
+
+        return response()->json(['success' => 'Notification sent successfully'], 200);
     }
 
     //to get all notification
@@ -26,8 +37,14 @@ class NotificationController extends Controller
         if (!$user) {
             return response()->json(['error' => 'User not found'], 404);
         }
+
         //This line fetches all notifications that have been sent to the user
-        $notifications=$user->notifications()->latest()->get();
+        $notifications=$user->notifications()->latest()->paginate(10);
+
+        //Check whether there are notifications or not
+        if ($notifications->isEmpty()) {
+            return response()->json(['message' => 'No notifications available'], 200);
+        }
         return response()->json($notifications);
     }
 
@@ -36,6 +53,10 @@ class NotificationController extends Controller
         $user = User::find($request->input('user_id'));
         if (!$user) {
             return response()->json(['error' => 'User not found'], 404);
+        }
+        //Check if there are any unread notifications
+        if($user->unreadNotifications->count()===0){
+            return response()->json(['message'=>'No unread notification to mark as read'],200);
         }
         //to put a mark as read
         $user->unreadNotifications->markAsRead();
