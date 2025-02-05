b62aad1c3d1c85c083ac51db037c6048147545ae
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-02-05 19:21:47 +0200

    authentication

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewerController;
+use App\Http\Controllers\Api\AccessTokenController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::post('/addreviewer', [ReviewerController::class,'addreviewer']); ////add reviewer
+
+Route::post('auth/access_token', [AccessTokenController::class, 'store'])->middleware('guest:sanctum');
