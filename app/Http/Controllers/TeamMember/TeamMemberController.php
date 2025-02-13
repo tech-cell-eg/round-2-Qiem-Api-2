7455008721121bf4d16653789882e3c32420e1f9
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-02-13 20:47:50 +0200

    show team members

---
+++ app/Http/Controllers/TeamMember/TeamMemberController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\TeamMember;
+use App\Http\Requests\StoreTeamMemberRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\TeamMember;
+use Illuminate\Http\JsonResponse;
+
+class TeamMemberController extends Controller
+{
+    public function store(StoreTeamMemberRequest $request): JsonResponse
+    {
+        $teamMember = TeamMember::create($request->validated());
+
+        return response()->json([
+            'message' => 'Team Member created successfully!',
+            'data' => $teamMember
+        ], 201);
+    }
+
+    public function index(): JsonResponse
+    {
+        $teamMembers = TeamMember::with(['inspector', 'reviewer'])->get();
+
+        return response()->json($teamMembers);
+    }
+}
